83c11961491693638415c9f1d18e070e5cc27ce0
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-07-23 12:59:15 +0200

    Finishing backend routes

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -6,7 +6,7 @@ const Account = require('../../models/Account');
 const ObjectId = require('mongodb').ObjectID;
 
 // @route     POST api/accounts
-// @desc      Create or update account
+// @desc      Create account
 // @access    Private
 router.post(
   '/',
@@ -26,8 +26,6 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).send({ errors: errors.array() });
     }
-    console.log('REQ.BODY => ', req.body);
-    console.log('REQ.USER.ID => ', req.user.id);
 
     const { title, titleNum } = req.body;
 
@@ -37,10 +35,54 @@ router.post(
     if (titleNum) accountFields.titleNum = titleNum;
 
     try {
-      // let account = await Account.findOne({ user: req.user.id });
-
       let account = new Account(accountFields);
       await account.save();
+      res.send(account);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route     PUT api/accounts/:accountId
+// @desc      Edit account
+// @access    Private
+router.put(
+  '/:accountId',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('titleNum', 'Account number is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const { title, titleNum } = req.body;
+
+    const accountFields = {};
+    accountFields.user = req.user.id;
+    if (title) accountFields.title = title;
+    if (titleNum) accountFields.titleNum = titleNum;
+
+    try {
+      const id = req.params.accountId;
+
+      account = await Account.findOneAndUpdate(
+        { _id: ObjectId(id) },
+        { $set: accountFields },
+        { new: true }
+      );
+
       res.send(account);
     } catch (err) {
       console.error(err.message);
@@ -54,12 +96,10 @@ router.post(
 // @access    Public
 router.get('/', async (req, res) => {
   try {
-    console.log(req.query.term);
     const userQuery = req.query.term.toLowerCase();
-    // const regExp = new RegExp(`.*${userQuery}.*`, `gi`);
     const accounts = await Account.find({
       title: { $regex: userQuery, $options: 'i' }
-    });
+    }).sort({ titleNum: 1 }); // sort ascending
     res.send(accounts);
   } catch (err) {
     console.error(err.message);
@@ -67,14 +107,128 @@ router.get('/', async (req, res) => {
   }
 });
 
-// @route     GET api/accounts/:account_id
+// @route     GET api/accounts/:accountId
 // @desc      Get account based on account id
 // @access    Public
-router.get('/:account_id', async (req, res) => {
+router.get('/:accountId', async (req, res) => {
   try {
-    const id = req.params.account_id;
-    const account = await Account.findOne({ _id: ObjectId(id) });
+    const id = req.params.accountId;
+    const account = await Account.findById({ _id: ObjectId(id) });
+
+    if (!account) {
+      return res.status(404).send({ msg: 'Account not found' });
+    }
+
     res.send(account);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({ msg: 'Account not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     DELETE api/accounts/:accountId
+// @desc      Delete account
+// @access    Private
+router.delete('/:accountId', auth, async (req, res) => {
+  try {
+    const id = req.params.accountId;
+    // await Account.findOneAndRemove({ _id: ObjectId(id) });
+    const account = await Account.findById({ _id: id });
+
+    if (!account) {
+      return res.status(404).send({ msg: 'Account not found' });
+    }
+
+    await account.remove();
+    res.send({ msg: 'Account successfully deleted!' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({ msg: 'Account not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     PUT api/accounts/meta/:accountId
+// @desc      Add account meta
+// @access    Private
+router.put(
+  '/meta/:accountId',
+  [
+    auth,
+    [
+      check('content', 'Account content is required')
+        .not()
+        .isEmpty(),
+      check('contentNum', 'Account number is required')
+        .not()
+        .isEmpty(),
+      check('details', 'Account details are required')
+        .not()
+        .isEmpty(),
+      check('tags', 'Tags are required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const id = req.params.accountId;
+    const { content, contentNum, details, tags } = req.body;
+
+    const newMeta = {
+      content,
+      contentNum,
+      details,
+      tags
+    };
+
+    try {
+      // Fetch account to which we want to add META to
+      const account = await Account.findOne({ _id: ObjectId(id) });
+
+      account.meta.unshift(newMeta);
+      await account.save();
+      res.send(account);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route     DELETE api/accounts/meta/accountId/:subId
+// @desc      Remove account meta item by index
+// @access    Private
+router.delete('/meta/:accountId/:subId', auth, async (req, res) => {
+  try {
+    const { subId, accountId } = req.params;
+    const account = await Account.findOne({ _id: ObjectId(accountId) });
+
+    // Pull out meta
+    const meta = account.meta.find(item => item.id === subId);
+
+    // Make sure that meta exists
+    if (!meta) {
+      return res.status(404).send({ msg: 'Meta information does not exist' });
+    }
+
+    // Finde index of meta id which matches the one which was clicked
+    const removeIndex = account.meta.map(item => item.id).indexOf(subId);
+
+    account.meta.splice(removeIndex, 1);
+
+    await account.save();
+
+    res.send(account.meta); // return all meta information
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
